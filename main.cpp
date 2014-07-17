6a6fa5fa58335142ce944bfec535b146810e485a
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-17 19:33:23 +0800

    add comment for readability and define constants scalability

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <cmath>
 #include <algorithm>
 #include <string>
+#define MAXINPUT 10000
+#define GRIDSIZE 100
 
 using namespace std;
 
@@ -16,25 +18,25 @@ struct Point{
 };
 struct Line{
 	int id;
-	vector<Point>lipoint;
+	vector<Point> lipoint;
 };
-struct grid_node{	//record this grid has which line and point
-	map<int,int>line_id;	//line -> record this line id
+struct grid_node{				//record the lines and points in this grid
+	map<int,int> line_id;		//line -> record this line id
 	vector<Point> grid_point;	//point ->record this obstacle's x and y
 };
 
+// Argument: 1.pointToRemove 2.LineInput gml file 3.PointInput gml file 4.LineOutput gml file
 int main(int argv,char *argc[]){
-
-	int PointToRemove = atoi(argc[1]);
-    FILE *LineInput,*PointInput,*LineOut;
-    char input[10000];
+	
+	int PointToRemove = atoi(argc[1]); // the point need to removed for porgram stop
+    FILE *LineInput, *PointInput, *LineOut;
+    char input[MAXINPUT];
     map<double, map<double, grid_node> > grid;	//record each has how many line and point
     vector<Point> obstacle_point;	//record Point from pointinput
     vector<Line> line,ans;
     Point ptmp;
     Line ltmp;
-    
-
+   
     LineInput=fopen(argc[2],"r");
     PointInput=fopen(argc[3],"r");
 	LineOut=fopen(argc[4],"w");
@@ -47,7 +49,6 @@ int main(int argv,char *argc[]){
 
 		int id = atoi(input);	//get the id	
 		while(fscanf(LineInput, "%s", input) != EOF){	//ignore gml format and its text
-
 			if(input[0] == '"' && input[1] == '>'){
 				break;
 			}
@@ -64,34 +65,33 @@ int main(int argv,char *argc[]){
 			}
 		}
 
-		grid[ (int)(ptmp.x / 100) * 100][ (int)(ptmp.y / 100) * 100].line_id[id] = 1;
+		grid[ (int)(ptmp.x / GRIDSIZE ) * GRIDSIZE ][ (int)(ptmp.y / GRIDSIZE ) * GRIDSIZE ].line_id[id] = 1;
 		ltmp.lipoint.push_back(ptmp);
 		ptmp.removable = 1;
 
 		while(fscanf(LineInput, "%s", input) != EOF){	//other point in this line
+		
 			if(input[0] == '<'){	
 					break;		//end of the point
 			}
 		
 			ptmp.x = atof(input);
 			for(int i=0; input[i] != '\0'; i++){
-				
 				if(input[i] == ','){
 					ptmp.y = atof(input+i+1);
 					break;
 				}
 			}
-
-			grid[(int)(ptmp.x / 100) * 100][(int)(ptmp.y / 100) * 100].line_id[id] = 1;
+			grid[(int)(ptmp.x / GRIDSIZE ) * GRIDSIZE ][(int)(ptmp.y / GRIDSIZE ) * GRIDSIZE ].line_id[id] = 1;
 			ltmp.lipoint.push_back(ptmp);
 		}
 
 		ltmp.lipoint[ltmp.lipoint.size() - 1].removable = 0;
 		line.push_back(ltmp);	//put line to vector
 	}
-	while(fscanf(PointInput,"%s", input) != EOF){	//read PointInput
+	while(fscanf(PointInput, "%s", input) != EOF){	//read PointInput
 		while(fscanf(LineInput,"%s",input) != EOF){	//ignore gml format and its text
-			if(input[0] == '"' && input[1] = ='>'){
+			if(input[0] == '"' && input[1] == '>'){
 				break;
 			}
 		}
@@ -104,7 +104,7 @@ int main(int argv,char *argc[]){
 			}
 
 		//add this obstacle to this grid
-		grid[ (int)(ptmp.x / 100) * 100][ (int)(ptmp.y / 100) * 100].grid_point.push_back(ptmp);
+		grid[ (int)(ptmp.x / GRIDSIZE ) * GRIDSIZE ][ (int)(ptmp.y / GRIDSIZE ) * GRIDSIZE ].grid_point.push_back(ptmp);
 		while(fscanf(LineInput,"%s",input) != EOF){	//read until to the gml end
 			if(input[0] == '<'){	
 					break;
@@ -119,14 +119,14 @@ int main(int argv,char *argc[]){
     	int max_x, min_x, max_y, min_y, tmpx, tmpy;
 
     	line_point_size=line[i].lipoint.size();
-    	max_x = min_x = (int)(line[i].lipoint[0].x / 100) * 100;	//calculate which is start grid
-    	max_y = min_y = (int)(line[i].lipoint[0].y / 100) * 100;
+    	max_x = min_x = (int)(line[i].lipoint[0].x / GRIDSIZE) * GRIDSIZE;	//calculate which is start grid
+    	max_y = min_y = (int)(line[i].lipoint[0].y / GRIDSIZE) * GRIDSIZE;
     	
     	int no_other = 1;
     	
     	for(int j = 0; i < line_point_size; j++){
-			tmpx= (int)(line[i].lipoint[j].x / 100) * 100;	//get the current point is in which grid
-			tmpy= (int)(line[i].lipoint[j].y / 100) * 100;
+			tmpx= (int)(line[i].lipoint[j].x / GRIDSIZE) * GRIDSIZE;	//get the current point is in which grid
+			tmpy= (int)(line[i].lipoint[j].y / GRIDSIZE) * GRIDSIZE;
 			
 			if(tmpx > max_x){	//check the new area whether has other object that may cause line and line cross
 				grid_node gtmp;
