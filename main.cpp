5a891be005fad254189455e764b92c9385936c5b
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-17 19:18:52 +0800

    coding style add more space for readability

--- main.cpp
+++ main.cpp
@@ -24,7 +24,8 @@ struct grid_node{	//record this grid has which line and point
 };
 
 int main(int argv,char *argc[]){
-    int PointToRemove = atoi(argc[1]);
+
+	int PointToRemove = atoi(argc[1]);
     FILE *LineInput,*PointInput,*LineOut;
     char input[10000];
     map<double, map<double, grid_node> > grid;	//record each has how many line and point
@@ -32,6 +33,8 @@ int main(int argv,char *argc[]){
     vector<Line> line,ans;
     Point ptmp;
     Line ltmp;
+    
+
     LineInput=fopen(argc[2],"r");
     PointInput=fopen(argc[3],"r");
 	LineOut=fopen(argc[4],"w");
@@ -39,85 +42,111 @@ int main(int argv,char *argc[]){
 		because the gml format is fixed, do not care what the text write
 		for each point, let grid to record this grid has which point and line
 	*/
-	while(fscanf(LineInput,"%s",input)!=EOF){	//read line
-		int id=atoi(input);	//get the id
-		while(fscanf(LineInput,"%s",input)!=EOF){	//ignore gml format and its text
-			if(input[0]=='"' && input[1]=='>'){
+
+	while(fscanf(LineInput, "%s", input) != EOF){	//read line
+
+		int id = atoi(input);	//get the id	
+		while(fscanf(LineInput, "%s", input) != EOF){	//ignore gml format and its text
+
+			if(input[0] == '"' && input[1] == '>'){
 				break;
 			}
 		}
-		ltmp.id=id;
+
+		ltmp.id = id;
 		ltmp.lipoint.clear();
-		ptmp.removable=0;	//each line of first point
-		ptmp.x=atof(input+2);	//read x
-		for(int i=2;input[i]!='\0';i++)	//read y
+		ptmp.removable = 0;	//each line of first point
+		ptmp.x = atof(input + 2);	//read x
+		for(int i=2; input[i] != '\0'; i++){	//read y
 			if(input[i]==','){
 				ptmp.y=atof(input+i+1);
 				break;
 			}
-		grid[(int)(ptmp.x/100)*100][(int)(ptmp.y/100)*100].line_id[id]=1;
-		ltmp.lipoint.push_back(ptmp);
-		ptmp.removable=1;
+		}
 
-		while(fscanf(LineInput,"%s",input)!=EOF){	//other point in this line
-			if(input[0]=='<')	break;		//end of the point
-			ptmp.x=atof(input);
-			for(int i=0;input[i]!='\0';i++)
-				if(input[i]==','){
-					ptmp.y=atof(input+i+1);
+		grid[ (int)(ptmp.x / 100) * 100][ (int)(ptmp.y / 100) * 100].line_id[id] = 1;
+		ltmp.lipoint.push_back(ptmp);
+		ptmp.removable = 1;
+
+		while(fscanf(LineInput, "%s", input) != EOF){	//other point in this line
+			if(input[0] == '<'){	
+					break;		//end of the point
+			}
+		
+			ptmp.x = atof(input);
+			for(int i=0; input[i] != '\0'; i++){
+				
+				if(input[i] == ','){
+					ptmp.y = atof(input+i+1);
 					break;
 				}
-			grid[(int)(ptmp.x/100)*100][(int)(ptmp.y/100)*100].line_id[id]=1;
+			}
+
+			grid[(int)(ptmp.x / 100) * 100][(int)(ptmp.y / 100) * 100].line_id[id] = 1;
 			ltmp.lipoint.push_back(ptmp);
 		}
-		ltmp.lipoint[ltmp.lipoint.size()-1].removable=0;
+
+		ltmp.lipoint[ltmp.lipoint.size() - 1].removable = 0;
 		line.push_back(ltmp);	//put line to vector
 	}
-	while(fscanf(PointInput,"%s",input)!=EOF){	//read PointInput
-		while(fscanf(LineInput,"%s",input)!=EOF){	//ignore gml format and its text
-			if(input[0]=='"' && input[1]=='>'){
+	while(fscanf(PointInput,"%s", input) != EOF){	//read PointInput
+		while(fscanf(LineInput,"%s",input) != EOF){	//ignore gml format and its text
+			if(input[0] == '"' && input[1] = ='>'){
 				break;
 			}
 		}
-		ptmp.x=atof(input+2);	//read x
-		for(int i=2;input[i]!='\0';i++)	//read y
-			if(input[i]==','){
-				ptmp.y=atof(input+i+1);
+
+		ptmp.x = atof(input+2);	//read x
+		for(int i=2; input[i] != '\0'; i++)	//read y
+			if(input[i] == ','){
+				ptmp.y = atof(input+i+1);
 				break;
 			}
+
 		//add this obstacle to this grid
-		grid[(int)(ptmp.x/100)*100][(int)(ptmp.y/100)*100].grid_point.push_back(ptmp);
-		while(fscanf(LineInput,"%s",input)!=EOF){	//read until to the gml end
-			if(input[0]=='<')	break;
+		grid[ (int)(ptmp.x / 100) * 100][ (int)(ptmp.y / 100) * 100].grid_point.push_back(ptmp);
+		while(fscanf(LineInput,"%s",input) != EOF){	//read until to the gml end
+			if(input[0] == '<'){	
+					break;
+			}
 		}
 	}
+
     /*delete the point*/
-    int line_size=line.size(),line_point_size;
-    for(int i=0;i<line_size;i++){
-    	int max_x,min_x,max_y,min_y,tmpx,tmpy;
+    int line_size=line.size(), line_point_size;
+    
+    for(int i=0; i < line_size; i++){
+    	int max_x, min_x, max_y, min_y, tmpx, tmpy;
+
     	line_point_size=line[i].lipoint.size();
-    	max_x=min_x=(int)(line[i].lipoint[0].x/100)*100;	//calculate which is start grid
-    	max_y=min_y=(int)(line[i].lipoint[0].y/100)*100;
-    	int no_other=1;
-    	for(int j=0;i<line_point_size;j++){
-			tmpx=(int)(line[i].lipoint[j].x/100)*100;	//get the current point is in which grid
-			tmpy=(int)(line[i].lipoint[j].y/100)*100;
-			if(tmpx>max_x){	//check the new area whether has other object that may cause line and line cross
+    	max_x = min_x = (int)(line[i].lipoint[0].x / 100) * 100;	//calculate which is start grid
+    	max_y = min_y = (int)(line[i].lipoint[0].y / 100) * 100;
+    	
+    	int no_other = 1;
+    	
+    	for(int j = 0; i < line_point_size; j++){
+			tmpx= (int)(line[i].lipoint[j].x / 100) * 100;	//get the current point is in which grid
+			tmpy= (int)(line[i].lipoint[j].y / 100) * 100;
+			
+			if(tmpx > max_x){	//check the new area whether has other object that may cause line and line cross
 				grid_node gtmp;
-				for(int k=min_y;k<=max_y;k++){
-					if(grid[tmpx][k].grid_point.size()>0 ||
-						(grid[tmpx][k].line_id.size()>0 && grid[tmpx][k].line_id[line[j].id]==0) ){
-						no_other=0;
+				for(int k = min_y; k <= max_y; k++){
+					if(grid[tmpx][k].grid_point.size() > 0 ||
+						( grid[tmpx][k].line_id.size() > 0 && grid[tmpx][k].line_id[line[j].id] == 0) ){
+						no_other = 0;
 					}
-					if(no_other)	max_x=tmpx;
+					if(no_other){
+							max_x=tmpx;
+					}
 				}
-			}else if(tmpx<min_x){
+			}else if(tmpx < min_x){
 			}
-			if(tmpy>max_y){
-			}else if(tmpy<min_y){
+			if(tmpy > max_y){
+			}else if(tmpy < min_y){
 			}
     	}
     }
+    
     fclose(LineInput);
     fclose(PointInput);
     fclose(LineOut);
